5bff512509de3e549bc138a9a253e5dacc7840c7
tomoya <tomo314159@akane.waseda.jp>
2020-11-04 05:22:32 +0900

    fix: update gcn code for graph data

--- gcn.py
+++ gcn.py
@@ -3,53 +3,41 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
 import gym
-import numpy as np
-import random
 
 args = {
     'mode': 'cui'
 }
 
+env = gym.make('gym_sumo:sumo-v0', **args)
+dataset = env.graph.getGraph()
+
 
 class Net(torch.nn.Module):
-    def __init__(self, env):
+    def __init__(self):
         super(Net, self).__init__()
-        dataset = env.observation
-        num_action = env.action_space[0][0].n + env.action_space[0][1].shape[0]
         self.conv1 = GCNConv(dataset.num_node_features, 16)
-        self.conv2 = GCNConv(16, num_action)
+        self.conv2 = GCNConv(16, dataset.num_classes)
     
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
         
         x = self.conv1(x, edge_index)
         x = F.relu(x)
-        # x = F.dropout(x, training=self.training)
+        x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
-        f = F.softmax(x, dim=1)
-        return f
+        
+        return F.log_softmax(x, dim=1)
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-env = gym.make('gym_sumo:sumo-v0', **args)
-obs = env.reset()
-model = Net(env).to(device)
-action = []
-for i in range(100):
-    tmp = 0
-    tmp2 = np.array([random.uniform(0.0, 1.0)], dtype=np.float32)
-    action.append((tmp, tmp2))
-obs, reward, done, _ = env.step(action)
-dataset = obs
-data = dataset.to(device)
-test = model.forward(data)
-print(test)
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+model = Net().to(device)
+data = dataset[0].to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-# model.train()  # モデルを訓練モードにする。
-# for epoch in range(200):
-#     optimizer.zero_grad()
-#     out = model(data)
-#     loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-#     loss.backward()
-#     optimizer.step()
+model.train()  # モデルを訓練モードにする。
+for epoch in range(200):
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
