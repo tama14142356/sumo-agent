f83aef30194ea19a235edbd270b668af1ed42e36
tomo <tomo314159@akane.waseda.jp>
2020-11-05 16:21:09 +0900

    feat: organize the directory structure
    ファイルがやたら増えて、ぐちゃぐちゃになっていたので、それぞれ、グループ化して
    ファイルとディレクトリ構成の整理をした。

--- gcn_sample/gcn.py
+++ gcn_sample/gcn.py
@@ -17,15 +17,15 @@ class Net(torch.nn.Module):
         super(Net, self).__init__()
         self.conv1 = GCNConv(dataset.num_node_features, 16)
         self.conv2 = GCNConv(16, dataset.num_classes)
-    
+
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        
+
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
-        
+
         return F.log_softmax(x, dim=1)
 
 
