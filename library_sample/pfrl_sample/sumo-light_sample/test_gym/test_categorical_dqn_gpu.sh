3b5ab26ad6a500ef761ae512ba3388a8b8c0cf16
tomo <tomo314159@akane.waseda.jp>
2020-11-15 08:25:02 +0900

    fix: rename outdir for pfrl_sample
    結果の格納先のディレクトリ名を変更

--- library_sample/pfrl_sample/sumo-light_sample/test_gym/test_categorical_dqn_gpu.sh
+++ library_sample/pfrl_sample/sumo-light_sample/test_gym/test_categorical_dqn_gpu.sh
@@ -4,7 +4,7 @@ set -Ceu
 
 project_root=$(git rev-parse --show-superproject-working-tree --show-toplevel | head -1)
 tempdir=$(mktemp -d)
-outdir=$project_root/results/$(basename $tempdir)
+outdir=$project_root/results/sumo-light/prfl_result/categorical_dqn/$(basename $tempdir)
 
 gpu="$1"
 
@@ -12,6 +12,6 @@ dir_name=$(dirname $0)
 cd $dir_name/../gym
 
 # gym/categorical_dqn
-CUDA_VISIBLE_DEVICES=$gpu python train_categorical_dqn_gym.py --outdir $outdir/categorical_dqn/train --gpu 0
-model=$(find $outdir/categorical_dqn/train -name "*_finish")
-CUDA_VISIBLE_DEVICES=$gpu python train_categorical_dqn_gym.py --demo --monitor --load $model --outdir $outdir/categorical_dqn/demo --gpu 0
+CUDA_VISIBLE_DEVICES=$gpu python train_categorical_dqn_gym.py --outdir $outdir/train --gpu 0
+model=$(find $outdir/train -name "*_finish")
+CUDA_VISIBLE_DEVICES=$gpu python train_categorical_dqn_gym.py --demo --monitor --load $model --outdir $outdir/demo --gpu 0
