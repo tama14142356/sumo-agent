740134fbb7213f471011b600b22aa60a5a21cf3c
tomoya <tomo314159@akane.waseda.jp>
2020-11-19 11:20:03 +0900

    fix: correct script
    - *_cpu.sh
      --eval-n-runsが二回指定されてしまっていたので、訂正

--- library_sample/pfrl_sample/sumo-light_sample/test_gym/test_dqn_cpu.sh
+++ library_sample/pfrl_sample/sumo-light_sample/test_gym/test_dqn_cpu.sh
@@ -12,6 +12,6 @@ cd $dir_name/../gym
 # gym/dqn
 python train_dqn_gym.py --steps 10000 --replay-start-size 50 --outdir $outdir/train --gpu -1
 model=$(find $outdir/train -name "best")
-python train_dqn_gym.py --eval-n-runs 1 --demo --monitor --load $model --eval-n-runs 100 --outdir $outdir/best_demo --gpu -1
+python train_dqn_gym.py --eval-n-runs 1 --demo --monitor --load $model --outdir $outdir/best_demo --gpu -1
 model=$(find $outdir/train -name "*_finish")
-python train_dqn_gym.py --eval-n-runs 1 --demo --monitor --load $model --eval-n-runs 100 --outdir $outdir/finish_demo --gpu -1
+python train_dqn_gym.py --eval-n-runs 1 --demo --monitor --load $model --outdir $outdir/finish_demo --gpu -1
