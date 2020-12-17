1f272eca6ddf2eefe6149d52b17279be1ba46073
tomo <tomo314159@akane.waseda.jp>
2020-12-18 05:35:59 +0900

    fix: correct import
    __init__におけるimport がうまく言っていなかったので訂正

--- library_sample/pfrl_sample/sumo-light_sample/src/experiments_sumo/__init__.py
+++ library_sample/pfrl_sample/sumo-light_sample/src/experiments_sumo/__init__.py
@@ -1,9 +1,9 @@
-from eval_sumo import eval_performance  # NOQA
+from experiments_sumo.eval_sumo import eval_performance  # NOQA
 
-from train_agent_batch_sumo import train_agent_batch  # NOQA
-from train_agent_batch_sumo import train_agent_batch_with_evaluation  # NOQA
+from experiments_sumo.train_agent_batch_sumo import train_agent_batch  # NOQA
+from experiments_sumo.train_agent_batch_sumo import train_agent_batch_with_evaluation
 
-from train_agent_sumo import train_agent  # NOQA
-from train_agent_sumo import train_agent_with_evaluation  # NOQA
+from experiments_sumo.train_agent_sumo import train_agent  # NOQA
+from experiments_sumo.train_agent_sumo import train_agent_with_evaluation  # NOQA
 
 # from pfrl.experiments.train_agent_async import train_agent_async  # NOQA
