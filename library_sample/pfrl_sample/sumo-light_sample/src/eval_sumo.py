b247a0acf47fc98109e9f5955d4523a70c8ff039
tomo <tomo314159@akane.waseda.jp>
2020-12-18 04:45:41 +0900

    feat: format code for import
    オリジナルのファイルと同じような関数の配置に変更＆import分の並べ方も
    同様にした

--- library_sample/pfrl_sample/sumo-light_sample/src/eval_sumo.py
+++ library_sample/pfrl_sample/sumo-light_sample/src/eval_sumo.py
@@ -1,8 +1,10 @@
 import logging
-import statistics
-import numpy as np
-import os
 import subprocess
+import os
+import statistics
+
+import numpy as np
+
 import pfrl
 
 
@@ -70,6 +72,40 @@ def _run_episodes_sumo(
             "evaluation episode %s length:%s R:%s", len(scores), episode_len, test_r
         )
     return scores
+
+
+def run_evaluation_episodes(
+    env,
+    agent,
+    n_steps,
+    n_episodes,
+    max_episode_len=None,
+    logger=None,
+):
+    """Run multiple evaluation episodes and return returns.
+
+    Args:
+        env (Environment): Environment used for evaluation
+        agent (Agent): Agent to evaluate.
+        n_steps (int): Number of timesteps to evaluate for.
+        n_episodes (int): Number of evaluation runs.
+        max_episode_len (int or None): If specified, episodes longer than this
+            value will be truncated.
+        logger (Logger or None): If specified, the given Logger object will be
+            used for logging results. If not specified, the default logger of
+            this module will be used.
+    Returns:
+        List of returns of evaluation runs.
+    """
+    with agent.eval_mode():
+        return _run_episodes_sumo(
+            env=env,
+            agent=agent,
+            n_steps=n_steps,
+            n_episodes=n_episodes,
+            max_episode_len=max_episode_len,
+            logger=logger,
+        )
 
 
 def _batch_run_episodes(
@@ -238,40 +274,6 @@ def batch_run_evaluation_episodes(
         )
 
 
-def run_evaluation_episodes(
-    env,
-    agent,
-    n_steps,
-    n_episodes,
-    max_episode_len=None,
-    logger=None,
-):
-    """Run multiple evaluation episodes and return returns.
-
-    Args:
-        env (Environment): Environment used for evaluation
-        agent (Agent): Agent to evaluate.
-        n_steps (int): Number of timesteps to evaluate for.
-        n_episodes (int): Number of evaluation runs.
-        max_episode_len (int or None): If specified, episodes longer than this
-            value will be truncated.
-        logger (Logger or None): If specified, the given Logger object will be
-            used for logging results. If not specified, the default logger of
-            this module will be used.
-    Returns:
-        List of returns of evaluation runs.
-    """
-    with agent.eval_mode():
-        return _run_episodes_sumo(
-            env=env,
-            agent=agent,
-            n_steps=n_steps,
-            n_episodes=n_episodes,
-            max_episode_len=max_episode_len,
-            logger=logger,
-        )
-
-
 def eval_performance(
     env, agent, n_steps, n_episodes, max_episode_len=None, logger=None
 ):
@@ -311,7 +313,6 @@ def eval_performance(
             max_episode_len=max_episode_len,
             logger=logger,
         )
-
     stats = dict(
         episodes=len(scores),
         mean=statistics.mean(scores),
